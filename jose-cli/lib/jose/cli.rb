bc60f348d794022ec7549969f550a89ef12796e4
jose luis <cortezluisjose@gmail.com>
2020-12-07 05:21:20 +0000

    Automatically backed up by Learn

---
+++ jose-cli/lib/jose/cli.rb
@@ -0,0 +1,8 @@
+require "jose/cli/version"
+
+module Jose
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ jose-cli/lib/jose/cli/version.rb
@@ -0,0 +1,5 @@
+module Jose
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
